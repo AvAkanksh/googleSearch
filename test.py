0ce743795c0fd61a5521eda09aca4347d39af0c2
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2021-09-27 03:22:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,13 +13,6 @@ for i in range(1,len(argv)):
 my_url = "https://www.google.com/search?q="+final_string
 # print("Your final url : \n"+url)
 
-
-#Title Text Block
-# print("GOOGLE SCRAPING USING PY3 AND BS4 \n")
-
-#Accepts Search Keywords from the User
-# keyword = input("ENTER SEARCH KEYWORDS: ")
-
 #Setting Query Parameters for Search 
 params = [('q',keyword)]
 
@@ -36,12 +29,7 @@ soup = bs4.BeautifulSoup(response.text,'lxml')
 titles = soup.find_all("h3")
 mydivs = soup.find_all("div", {"class": "IZ6rdc"})
 # <div class="IZ6rdc">Mahatma Gandhi</div>
-#Result is Displayed
 
-# print("\nDISPLAYING FIRST PAGE RESULT TITLES FOR " + keyword + "\n")
 print(mydivs)
 print(soup)
-# x = 0; # counter variable
-# for title in titles: #loop to display titles
-# 	x=x+1 # counter updated
 # 	print( str(x) + ". " + title.get_text() )
